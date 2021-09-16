e2cde7a2c36f78752fa601adc3affab5c1ad898e
sydneyw-cyber <sydneyw521@gmail.com>
2021-09-16 09:15:04 -0500

    final commit/ final updates

--- Develop/index.js
+++ Develop/index.js
@@ -2,6 +2,7 @@
 var time = $('#currentDay')
 var hour = $('.hour')
 var saveBtn = $('.saveBtn')
+var saveBtn0 = $('.saveBtn0')
 var saveBtn1 = $('#saveBtn1')
 var saveBtn2 = $('#saveBtn2')
 var saveBtn3 = $('#saveBtn3')
@@ -15,21 +16,22 @@ var saveBtn10 = $('#saveBtn10')
 
 
 // When page is refeshed the calue of the input wont change
-$(saveBtn1).siblings('input').val(localStorage.getItem('event-value1'))
-$(saveBtn2).siblings('input').val(localStorage.getItem('event-value2'))
-$(saveBtn3).siblings('input').val(localStorage.getItem('event-value3'))
-$(saveBtn4).siblings('input').val(localStorage.getItem('event-value4'))
-$(saveBtn5).siblings('input').val(localStorage.getItem('event-value5'))
-$(saveBtn6).siblings('input').val(localStorage.getItem('event-value6'))
-$(saveBtn7).siblings('input').val(localStorage.getItem('event-value7'))
-$(saveBtn8).siblings('input').val(localStorage.getItem('event-value8'))
-$(saveBtn9).siblings('input').val(localStorage.getItem('event-value9'))
-$(saveBtn10).siblings('input').val(localStorage.getItem('event-value10'))
+$(saveBtn0).siblings('input').val(localStorage.getItem('event-input0'))
+$(saveBtn1).siblings('input').val(localStorage.getItem('event-input1'))
+$(saveBtn2).siblings('input').val(localStorage.getItem('event-input2'))
+$(saveBtn3).siblings('input').val(localStorage.getItem('event-input3'))
+$(saveBtn4).siblings('input').val(localStorage.getItem('event-input4'))
+$(saveBtn5).siblings('input').val(localStorage.getItem('event-input5'))
+$(saveBtn6).siblings('input').val(localStorage.getItem('event-input6'))
+$(saveBtn7).siblings('input').val(localStorage.getItem('event-input7'))
+$(saveBtn8).siblings('input').val(localStorage.getItem('event-input8'))
+$(saveBtn9).siblings('input').val(localStorage.getItem('event-input9'))
+$(saveBtn10).siblings('input').val(localStorage.getItem('event-input10'))
 
 
 // sets current date
-var today = moment().format('dddd MMM Do, YYYY')
-$(time).text(today)
+var todayDate = moment().format('dddd MMM Do, YYYY')
+$(time).text(todayDate)
 
 //Color codes based on past, present, and future events
 var currentHour = moment().format('ha')
@@ -53,85 +55,94 @@ for (var i=0; i < hour.length; i++) {
 
 // adding input to calendar once the button is clicked
 
-$(saveBtn1).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
+$(saveBtn0).on('click', function(event) {
+    var eventInput = $(event.target).siblings('input').val()
     
-    localStorage.setItem('event-value1', eventValue)
-    localStorage.getItem('event-value1')
-    $(event.target).siblings('input').text(eventValue)
+    localStorage.setItem('event-input0', eventInput)
+    localStorage.getItem('event-input0')
+    $(event.target).siblings('input').text(eventInput)
+    
+})
+
+$(saveBtn1).on('click', function(event) {
+    var eventInput = $(event.target).siblings('input').val()
+    
+    localStorage.setItem('event-input1', eventInput)
+    localStorage.getItem('event-input1')
+    $(event.target).siblings('input').text(eventInput)
     
 })
 
 $(saveBtn2).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
+    var eventInput = $(event.target).siblings('input').val()
     
-    localStorage.setItem('event-value2', eventValue)
-    localStorage.getItem('event-value2')
-    $(event.target).siblings('input').text(eventValue)
+    localStorage.setItem('event-input2', eventInput)
+    localStorage.getItem('event-input2')
+    $(event.target).siblings('input').text(eventInput)
     
 })
 
 $(saveBtn3).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
-    localStorage.setItem('event-value3', eventValue)
-    localStorage.getItem('event-value3')
-    $(event.target).siblings('input').text(eventValue)
+    var eventInput = $(event.target).siblings('input').val()
+    localStorage.setItem('event-input3', eventInput)
+    localStorage.getItem('event-input3')
+    $(event.target).siblings('input').text(eventInput)
     
 })
 
 $(saveBtn4).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
-    localStorage.setItem('event-value4', eventValue)
-    localStorage.getItem('event-value4')
-    $(event.target).siblings('input').text(eventValue)
+    var eventInput = $(event.target).siblings('input').val()
+    localStorage.setItem('event-input4', eventInput)
+    localStorage.getItem('event-input4')
+    $(event.target).siblings('input').text(eventInput)
     
 })
 
 $(saveBtn5).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
-    localStorage.setItem('event-value5', eventValue)
-    localStorage.getItem('event-value5')
-    $(event.target).siblings('input').text(eventValue)
+    var eventInput = $(event.target).siblings('input').val()
+    localStorage.setItem('event-input5', eventInput)
+    localStorage.getItem('event-input5')
+    $(event.target).siblings('input').text(eventInput)
     
 })
 
 $(saveBtn6).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
-    localStorage.setItem('event-value6', eventValue)
-    localStorage.getItem('event-value6')
-    $(event.target).siblings('input').text(eventValue)
+    var eventInput = $(event.target).siblings('input').val()
+    localStorage.setItem('event-input6', eventInput)
+    localStorage.getItem('event-input6')
+    $(event.target).siblings('input').text(eventInput)
     
 })
 
 $(saveBtn7).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
-    localStorage.setItem('event-value7', eventValue)
-    localStorage.getItem('event-value7')
-    $(event.target).siblings('input').text(eventValue)
+    var eventInput = $(event.target).siblings('input').val()
+    localStorage.setItem('event-input7', eventInput)
+    localStorage.getItem('event-input7')
+    $(event.target).siblings('input').text(eventInput)
 
 })
 
 $(saveBtn8).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
-    localStorage.setItem('event-value8', eventValue)
-    localStorage.getItem('event-value8')
-    $(event.target).siblings('input').text(eventValue)
+    var eventInput = $(event.target).siblings('input').val()
+    localStorage.setItem('event-input8', eventInput)
+    localStorage.getItem('event-input8')
+    $(event.target).siblings('input').text(eventInput)
 
 })
 
 $(saveBtn9).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
-    localStorage.setItem('event-value9', eventValue)
-    localStorage.getItem('event-value9')
-    $(event.target).siblings('input').text(eventValue)
+    var eventInput = $(event.target).siblings('input').val()
+    localStorage.setItem('event-input9', eventInput)
+    localStorage.getItem('event-input9')
+    $(event.target).siblings('input').text(eventInput)
 
 })
 
 $(saveBtn10).on('click', function(event) {
-    var eventValue = $(event.target).siblings('input').val()
-    localStorage.setItem('event-value10', eventValue)
-    localStorage.getItem('event-value10')
-    $(event.target).siblings('input').text(eventValue)
+    var eventInput = $(event.target).siblings('input').val()
+    localStorage.setItem('event-input10', eventInput)
+    localStorage.getItem('event-input10')
+    $(event.target).siblings('input').text(eventInput)
 
 })
 
